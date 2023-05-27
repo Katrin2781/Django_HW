71074810431992f45e7a91f64d6849405e970075
Ekaterina Komarova <borodatova@.bk.ru>
2023-05-27 21:02:13 +0400

    update setting.py

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
-        'USER': 'postgres',
-        'PASSWORD': 'Rjvfhjdf81',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
